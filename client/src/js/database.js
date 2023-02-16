1335a21c7fb6e6109d71657ef350cd679597f757
Riley <94302477+Rilaey@users.noreply.github.com>
2023-02-16 14:38:46 -0500

    web app currently functions. still working on getting the service work to activate to allow for install.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,55 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    }
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // create connection to database
+  const jateDB = await openDB("jate", 1);
+
+  // create new transaction and the privileges we want
+  const transact = jateDB.transaction("jate", "readwrite");
+
+  // open up object store we want
+  const store = transact.objectStore("jate");
+
+  // add() to add data to db
+  const request = store.add({ content: content} );
+
+  // confirm the request worked
+  const result = await request;
+  console.log("Results!", result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // create connection to database
+  const jateDB = await openDB("jate", 1);
+
+  // create new transaction and the privileges we want
+  const transact = jateDB.transaction("jate", "readonly");
+
+  // open up object store we want
+  const store = transact.objectStore("jate");
+
+  // getAll() built in method to get all data in db
+  const request = store.getAll();
+
+  // confirm the request worked
+  const result = await request;
+  console.log("Results!", result);
+  return result;
+};
 
 initdb();
